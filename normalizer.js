48fa4824b22bfea887657a1f583080d4dcde9e01
Nick Clarkson <nikclarkson@gmail.com>
2012-05-10 22:47:56 +0100

    n and b added

---
+++ normalizer.js
@@ -0,0 +1,67 @@
+			var n = (function(){
+				
+				var env = BrowserDetect.browser;
+
+				switch(env){
+					
+					case 'Chrome':
+					case 'safari': 
+						
+						env = 'Webkit';
+
+					break;
+
+				}
+
+				var cnf = {
+					
+					Webkit: {
+						
+						transitionEnd: 'webkitAnimationEnd',
+						transform: '-webkit-transform'
+
+					},
+
+					Gecko: {
+						
+						transitionEnd: 'animationend',
+						transform: '-moz-transform'
+
+					},
+
+					Explorer: {
+						
+						transitionEnd: 'MSAnimationEnd',
+						transform: '-ms-transform'
+
+					}
+
+				} 
+
+				return {
+
+					property: function(p){
+						
+						return cnf[env][p] || false;
+
+					},
+
+					css: function(c){
+						
+
+
+					},
+					
+					browser: function(){
+						
+						return env;
+
+					},
+
+					trace: function(){
+						
+					}
+
+				}
+
+			})();
